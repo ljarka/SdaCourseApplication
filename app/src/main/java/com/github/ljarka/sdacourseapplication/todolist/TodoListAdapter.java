1bcbfded7b843bf8a5c1939f832d27c6c566a12e
Lukasz Jarka <lukasz.jarka@allegrogroup.com>
2017-02-23 20:51:31 +0100

    Removing items from todolist

--- app/src/main/java/com/github/ljarka/sdacourseapplication/todolist/TodoListAdapter.java
+++ app/src/main/java/com/github/ljarka/sdacourseapplication/todolist/TodoListAdapter.java
@@ -4,6 +4,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.TextView;
 
 import com.github.ljarka.sdacourseapplication.R;
@@ -13,7 +16,12 @@ import java.util.List;
 
 public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.MyViewHolder> {
 
-    List<String> items = new ArrayList<>();
+    private List<TodoListItem> items = new ArrayList<>();
+    private OnItemCheckStateChanged checkListener;
+
+    public void setCheckListener(OnItemCheckStateChanged checkListener) {
+        this.checkListener = checkListener;
+    }
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -24,7 +32,32 @@ public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.MyView
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        holder.textView.setText(items.get(position));
+
+        final TodoListItem listItem = items.get(position);
+        holder.textView.setText(listItem.getText());
+        holder.checkBox.setChecked(listItem.isChecked());
+        holder.checkBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                listItem.setChecked(isChecked);
+                if (checkListener != null) {
+                    checkListener.onItemCheckStateChanged(getCheckedItemsCount());
+                }
+            }
+        });
+    }
+
+    private int getCheckedItemsCount() {
+        int count = 0;
+
+        for (TodoListItem item : items) {
+            if (item.isChecked()) {
+                count++;
+            }
+        }
+
+        return count;
     }
 
     @Override
@@ -33,16 +66,34 @@ public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.MyView
     }
 
     public void addItem(String item) {
-        items.add(item);
+        items.add(new TodoListItem(item));
+        notifyDataSetChanged();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
         TextView textView;
+        CheckBox checkBox;
 
         public MyViewHolder(View itemView) {
             super(itemView);
             textView = (TextView) itemView.findViewById(R.id.item_text);
+            checkBox = (CheckBox) itemView.findViewById(R.id.checkbox);
+        }
+
+    }
+
+    public void deleteAllCheckedItems() {
+        List<TodoListItem> newListItems = new ArrayList<>();
+        for (int i = 0; i < items.size(); i++) {
+            if (!items.get(i).isChecked()) {
+                newListItems.add(items.get(i));
+            }
+        }
+        items = newListItems;
+        notifyDataSetChanged();
+        if (checkListener != null) {
+            checkListener.onItemCheckStateChanged(getCheckedItemsCount());
         }
     }
 }
